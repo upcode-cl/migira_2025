3e76e8559e3cb3cdcdc5d72dad9544519cd6fdf6
Mario Gutierrez <mario@upcode.cl>
2025-09-18 12:52:20 -0300

    ajustes en texto para programas regulares

--- components/Destinos.tsx
+++ components/Destinos.tsx
@@ -27,6 +27,8 @@ const Destinos = () => {
       <h2 className="text-[50px] text-[#58167D] flex justify-center items-center text-center pl-5 pr-5 font-bold">
         Nos Especializamos en Programas Turísticos
       </h2>
+      <h3 className="text-[50px] text-[#58167D] flex justify-center items-center text-center pl-5 pr-5">
+        para quienes buscan servicios exclusivos</h3>
       <div className="grid grid-col-1 lg:grid-cols-3 md:grid-cols-2 gap-10 pl-4 pr-4 lg:pl-28  lg:pr-28 pt-[50px] ">
         {programas.map((programa: Program) => (
           <Destinos_info
